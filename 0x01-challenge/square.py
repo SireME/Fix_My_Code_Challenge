d6a9b2c92dd6a7d6ac3eef2b54ea4b63ba10ce3e
Mbang Ernest N <mbangernest@yahoo.com>
2023-10-15 10:45:38 +0100

    add docs to square

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,12 +1,20 @@
 #!/usr/bin/python3
+"""
+module that defines a square
+"""
 
 
 class square():
-
+    """
+    class that defines a square
+    """
     width = 0
     height = 0
 
     def __init__(self, *args, **kwargs):
+        """
+        initialise the square
+        """
         for key, value in kwargs.items():
             setattr(self, key, value)
 
@@ -15,14 +23,22 @@ class square():
         return self.width * self.width
 
     def PermiterOfMySquare(self):
+        """
+        calculate perimeter of square
+        """
         return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
+        """
+        string representation od square
+        """
         return "{}/{}".format(self.width, self.height)
 
 
 if __name__ == "__main__":
-
+    """
+    compute square
+    """
     s = square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
